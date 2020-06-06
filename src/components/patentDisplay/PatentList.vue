63002c2caa30e1d72f3b7d10b3ad6dc5709c8c3e
chinchillawang <chinchilla@aliyun.com>
2020-06-06 12:09:51 +0800

    登录、注册跳转微调

--- src/components/patentDisplay/PatentList.vue
+++ src/components/patentDisplay/PatentList.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="nav-bar">
-    <el-menu
-      :default-active="'/home'"
-      class="el-menu-demo"
-      mode="horizontal"
-      router=""
-      active-text-color="#25567A">
-      <el-menu-item v-for="(item, i) in navList" :index="item.url" :key="i">
-        {{ item.name }}
-      </el-menu-item>
-    </el-menu>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NavMenu',
+  name: 'PatentList',
   data () {
     return {
-      navList: [
-        { name: '首页', url: '/home' },
-        { name: '专利列表', url: '/patentList' }
-      ]
+      result: []
     }
   },
+  mounted () {
+    var url = '/patent/add'
+    var _this = this
+    this.axios.get(url)
+      .then(function (response) {
+        if (response.status === 200) {
+          _this.result = response.data
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  },
   methods: {
-    handleSelect (key, keyPath) {
-
+    handleClick (tab, event) {
+      console.log(tab, event)
     }
   }
 }
